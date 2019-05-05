dede5058bcae1626a2bb9b4be3b8f25f4aa2d88c
Adrian Soluch <adrian@soluch.us>
2019-05-05 17:48:41 +0200

    .

--- block.js
+++ block.js
@@ -1,3 +1,5 @@
+const SHA256 = require('crypto-js/sha256')
+
 class Block {
 
   constructor(timestamp, lastHash, hash, data) {
@@ -22,8 +24,12 @@ class Block {
   static mineBlock(lastBlock, data) {
     const timestamp = Date.now()
     const lastHash = lastBlock.hash
-    const hash = 'TODO'
+    const hash = Block.hash(timestamp, lastHash, data)
     return new this(timestamp, lastHash, hash, data)
+  }
+
+  static hash(timestamp, lastHash, data) {
+    return SHA256(`${timestamp}${lastHash}${data}`).toString()
   }
 }
 
