39f08c90926c3be4b0e887369bf2263cef8962bb
Adrian Soluch <adrian@soluch.us>
2019-05-12 17:45:01 +0200

    #26

--- app/index.js
+++ app/index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const Blockchain = require('../blockchain')
+const P2pServer = require('./p2p-server')
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001
 
 const app = express()
 const bc = new Blockchain()
+const p2pServer = new P2pServer(bc)
 
 app.use(bodyParser.json())
 
@@ -23,3 +25,4 @@ app.post('/mine', (req, res) => {
 app.listen(HTTP_PORT, () => {
   console.log(`listening on port ${HTTP_PORT}`)
 })
+p2pServer.listen()
--- app/p2p-server.js
+++ app/p2p-server.js
@@ -1,4 +1,4 @@
-// $ HTTP_PORT=3002 P2P_PORT=5003 PEERS=ws://localhost:5001,ws:localhost:5002 npm run dev
+// $ HTTP_PORT=3002 P2P_PORT=5003 PEERS=ws://localhost:5001,ws://localhost:5002 npm run dev
 const Websocket = require('ws')
 
 const P2P_PORT = process.env.P2P_PORT || 5001
@@ -13,6 +13,9 @@ class P2pServer {
   listen() {
     const server = new Websocket.Server({port: P2P_PORT})
     server.on('connection', socket => this.connectSocket(socket))
+
+    this.connectToPeers()
+
     console.log(`Listening for peer2peer connections on ${P2P_PORT}`)
   }
 
@@ -20,4 +23,16 @@ class P2pServer {
     this.sockets.push(socket)
     console.log('Socket connected')
   }
+
+  connectToPeers() {
+    peers.forEach(peer => {
+      //ws://localhost:5001 || ws://192.168.1.11:5001
+      const socket = new Websocket(peer)
+      socket.on('open', () => {
+        this.connectSocket(socket)
+      })
+    })
+  }
 }
+
+module.exports = P2pServer
