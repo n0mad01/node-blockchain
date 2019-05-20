8643e07b26b00ef7a531075c8246e45dde20019c
Adrian Soluch <adrian@soluch.us>
2019-05-20 20:12:45 +0200

    #36 wallet init

---
+++ wallet/index.js
@@ -0,0 +1,18 @@
+const {INITIAL_BALANCE} = require('../config')
+
+class Wallet {
+  constructor() {
+    this.balance = INITIAL_BALANCE 
+    this.keyPair = null
+    this.publicKey = null
+  }
+
+
+  toString() {
+    return `Wallet - 
+      publicKey: ${this.publicKey.toString()}
+      balance: ${this.balance}`
+  }
+}
+
+module.export = Wallet
