8029a296fba9b2b636f2833af62000a82b3526de
Adrian Soluch <adrian@soluch.us>
2019-05-26 11:35:36 +0200

    #42

--- wallet/transaction.js
+++ wallet/transaction.js
@@ -38,6 +38,15 @@ class Transaction {
       signature: senderWallet.sign(ChainUtil.hash(transaction.outputs))
     }
   }
+
+  static verifyTransaction(transaction) {
+
+    return ChainUtil.verifysignature(
+      transaction.input.address,
+      transaction.input.signature
+      ChainUtil.hash(transaction.outputs)
+    )
+  }
 }
 
 module.exports = Transaction
