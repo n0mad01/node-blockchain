b70ff3d62ef04b2af520554e822cdb3381112642
Adrian Soluch <adrian@soluch.us>
2019-06-01 12:40:08 +0200

    #60 reward transactions

--- wallet/index.js
+++ wallet/index.js
@@ -36,6 +36,13 @@ class Wallet {
 
     return transaction
   }
+
+  static blockchainWallet() {
+    const blockchainWallet = new this()
+    blockchainWallet.address = 'blockchain-wallet'
+
+    return blockchainWallet
+  }
 }
 
 module.exports = Wallet
--- wallet/transaction.js
+++ wallet/transaction.js
@@ -1,4 +1,5 @@
 const ChainUtil = require('../chain-util')
+const {MINING_REWARD} = require('../config')
 
 class Transaction {
 
@@ -26,15 +27,38 @@ class Transaction {
     return this
   }
 
-  static newTransaction(senderWallet, recipient, amount) {
+  static transactionWithOutputs(senderWallet, outputs) {
     const transaction = new this()
+      
+    transaction.outputs.push(...outputs)
+    Transaction.signTransaction(transaction, senderWallet)
+
+    return transaction
+  }
+
+
+  static newTransaction(senderWallet, recipient, amount) {
+    //const transaction = new this()
 
     if(amount > senderWallet.balance) {
       console.log(`Amount: ${amount} exceeds balance`)
       return
     }
 
-    transaction.outputs.push(...[
+    /*transaction.outputs.push(...[
+      {
+        amount: senderWallet.balance - amount, 
+        address: senderWallet.publicKey
+      },
+      {
+        amount,
+        address: recipient
+      }
+    ])*/
+    //Transaction.signTransaction(transaction, senderWallet)
+    //return transaction
+
+    return Transaction.transactionWithOutputs(senderWallet, [
       {
         amount: senderWallet.balance - amount, 
         address: senderWallet.publicKey
@@ -44,9 +68,13 @@ class Transaction {
         address: recipient
       }
     ])
-    Transaction.signTransaction(transaction, senderWallet)
+  }
 
-    return transaction
+  static rewardTransaction(minerWallet, blockchainWallet) {
+    return Transaction.transactionWithOutputs(blockchainWallet, [{
+      amount: MINING_REWARD,
+      address: minerWallet.publicKey
+    }]) 
   }
 
   static signTransaction(transaction, senderWallet) {
