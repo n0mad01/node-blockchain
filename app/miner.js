2ac16f0803a6fc553e196078b59e110f75e6f200
Adrian Soluch <adrian@soluch.us>
2019-06-02 13:46:46 +0200

    #64 mine transactions endpoint added

--- app/miner.js
+++ app/miner.js
@@ -12,7 +12,7 @@ class Miner {
 
   mine() {
     const validTransactions = this.transactionPool.validTransactions()  
-    validTransaction.push(Transaction.rewardTransaction(this-wallet, Wallet.blockchainWallet()))
+    validTransactions.push(Transaction.rewardTransaction(this.wallet, Wallet.blockchainWallet()))
     const block = this.blockchain.addBlock(validTransactions)
     this.p2pServer.syncChains()
     this.transactionPool.clear()
